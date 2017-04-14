b1fe9f1161905994414c065646f870985fd92f04
Zoë Blade <zoe@bytenoise.co.uk>
2017-04-14 09:09:00 +0100

    Start work on reading out the font, for testing purposes

---
+++ fontread.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void readFont(FILE *inputFilePointer, FILE *outputFilePointer) {
+	int character;
+	int i;
+
+	while ((character = getc(inputFilePointer)) != EOF) {
+		for (i = 0; i < 8; i++) {
+			if (character & (1 << i)) {
+				putc('.', outputFilePointer);
+			} else {
+				putc(' ', outputFilePointer);
+			}
+
+			putc('\n', outputFilePointer);
+		}
+	}
+}
+
+int main(int argc, char *argv[]) {
+	FILE *filePointer;
+
+	if (argc == 1) {
+		return 0; /* Only work with named files, not stdin */
+	} else {
+		while (--argc > 0) {
+			filePointer = fopen(*++argv, "r");
+
+			if (filePointer == NULL) {
+				continue;
+			}
+
+			readFont(filePointer, stdout);
+			fclose(filePointer);
+		}
+	}
+
+	return 0;
+}
