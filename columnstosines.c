ef11bf70a6482a1f8b95e2124727be42bc94917b
Zoë Blade <zoe@bytenoise.co.uk>
2017-04-15 08:59:29 +0100

    Code more...
    I was going to loop through the rows and cols to get the oscillator
    on/off array, then loop through the number of iterations per pixel
    (the pixel width in samples) to output that combination...  but "col"
    already *is* that oscillator on/off array...

--- columnstosines.c
+++ columnstosines.c
@@ -6,19 +6,23 @@ a series of sine waves.  */
 #include <stdint.h>
 #include <stdio.h>
 
-uint8_t byte;
+int i;
 int col;
 int row;
+uint8_t out;
 
 uint8_t charset[2048];
 
 void convert(FILE *inputFilePointer, FILE *outputFilePointer) {
 	/* Loop through each column of the bitmap */
 	while ((col = getc(inputFilePointer)) != EOF) {
+		out = 0;
+
 		/* Work out which oscillators are on for this column */
 		for (row = 0; row < 8; row++) {
 			if (col & (1 << (7 - col))) {
-				byte |= 1 << row;
+				/* Each sine wave should be 1/6th volume, for mixing with headroom */
+				out += 6 / x;
 			}
 		}
 	}
