b77404219cb5b8342620cf38042b40034dc83b8c
Zoë Blade <zoe@bytenoise.co.uk>
2017-04-15 08:53:58 +0100

    Start rewriting...

--- columnstosines.c
+++ columnstosines.c
@@ -6,29 +6,20 @@ a series of sine waves.  */
 #include <stdint.h>
 #include <stdio.h>
 
-int c;
-
 uint8_t byte;
-int charOffset;
 int col;
 int row;
 
 uint8_t charset[2048];
 
 void convert(FILE *inputFilePointer, FILE *outputFilePointer) {
-	while ((c = getc(inputFilePointer)) != EOF) {
-		charOffset = 8 * c;
-
-		for (col = 0; col < 8; col++) {
-			byte = 0;
-
-			for (row = 0; row < 8; row++) {
-				if (charset[charOffset + row] & (1 << (7 - col))) {
-					byte |= 1 << row;
-				}
+	/* Loop through each column of the bitmap */
+	while ((col = getc(inputFilePointer)) != EOF) {
+		/* Work out which oscillators are on for this column */
+		for (row = 0; row < 8; row++) {
+			if (col & (1 << (7 - col))) {
+				byte |= 1 << row;
 			}
-
-			putc(byte, outputFilePointer);
 		}
 	}
 }
