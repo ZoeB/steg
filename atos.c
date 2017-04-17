46f68ee538dd36854da811c4ece90d07882bb48d
Zoë Blade <zoe@bytenoise.co.uk>
2017-04-17 15:56:32 +0100

    As a guess, increment the pointer, not its value

--- atos.c
+++ atos.c
@@ -197,7 +197,8 @@ int main(int argc, char *argv[]) {
 	argv += argc - 1;
 
 	while (*argv != '\0') {
-		c = *argv[0]++;
+		c = *argv[0];
+		argv++;
 
 		/* Only display printable ASCII characters */
 		if (c < 32 || c > 126) {
